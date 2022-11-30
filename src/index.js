1a895bf4edc82d6e177c9706abed2ff74d040a9e
bablu22 <mdbablu22.bablu22@gmail.com>
2022-11-30 23:01:19 +0600

    packages update

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import { ConfigProvider } from "antd";
 
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
